65509ffb26b274a5d1de5fab7149a07b0620e881
Hiroki Konishi <re_lastle@yahoo.co.jp>
2019-05-17 15:06:24 +0900

    Rename:

--- shell/pmy.zsh
+++ shell/pmy.zsh
@@ -1,6 +1,6 @@
 # test environment path variable
 export PATH="${GOPATH}/src/github.com/relastle/pmy:${PATH}"
-export PMY_CONFIG_PATH="${GOPATH}/src/github.com/relastle/pmy/resources/pmy_config.json"
+export PMY_CONFIG_PATH="${GOPATH}/src/github.com/relastle/pmy/resources/pmy_rules.json"
 
 pmy-widget() {
     local buffer_left=${LBUFFER}
