1b6e2ae44ad2dbfbff0331c3aeeba1a18180db51
Hiroki Konishi <relastle@gmail.com>
2019-06-12 16:28:12 +0900

    Update: implement auto-tag elimination, and implement after command excecution against fzf result

--- shell/pmy.zsh
+++ shell/pmy.zsh
@@ -1,3 +1,4 @@
+#!/usr/bin/env zsh
 # test environment path variable
 export PATH="${GOPATH}/src/github.com/relastle/pmy:${PATH}"
 export PMY_CONFIG_PATH="${GOPATH}/src/github.com/relastle/pmy/resources/pmy_rules_neo.json"
@@ -10,6 +11,7 @@ pmy-widget() {
 
     # get output from pmy
     local out="$(pmy --bufferLeft=${buffer_left} --bufferRight=${buffer_right} 2>/dev/null)"
+    echo $out
 
     if [[ -z $out  ]] then
         echo "No rule was matched"
@@ -18,7 +20,19 @@ pmy-widget() {
 
         # get result from fzf
         local fzf_res=$(echo "${__pmy_out_sources}" | fzf -0 -1)
-        LBUFFER="${__pmy_out_buffer_left}${fzf_res}"
+        # get tag
+        local tag=$(echo ${fzf_res} | awk '{print $1}')
+        # get rest statement
+        local fzf_res_rest=$(echo ${fzf_res} | awk '{for(i=2;i<NF;i++){printf("%s%s",$i,OFS=" ")}print $NF}')
+        # get after command
+        local after_cmd_variable="__pmy_out_${tag}_after"
+        local after_cmd=$(eval echo \$$after_cmd_variable)
+        echo ${tag}
+        echo ${after_cmd_variable}
+        echo ${fzf_res_rest}
+        echo ${after_cmd}
+        local res=$(echo ${fzf_res_rest} | eval ${after_cmd})
+        LBUFFER="${__pmy_out_buffer_left}${res}"
         RBUFFER="${__pmy_out_buffer_right}"
     fi
     zle reset-prompt
