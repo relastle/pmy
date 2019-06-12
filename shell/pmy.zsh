8eb6c4947a7ab2e020613da9697648d06dfbede2
Hiroki Konishi <relastle@gmail.com>
2019-06-12 12:33:29 +0900

    Update: add some comment

--- shell/pmy.zsh
+++ shell/pmy.zsh
@@ -4,14 +4,21 @@ export PMY_CONFIG_PATH="${GOPATH}/src/github.com/relastle/pmy/resources/pmy_rule
 export PMY_DELIMITER=':::'
 
 pmy-widget() {
+    # get current buffer information
     local buffer_left=${LBUFFER}
     local buffer_right=${RBUFFER}
+
+    # get output from pmy
     local out="$(pmy --bufferLeft=${buffer_left} --bufferRight=${buffer_right} 2>/dev/null)"
+
     if [[ -z $out  ]] then
         echo "No rule was matched"
     else
         eval ${out}
-        LBUFFER="${__pmy_out_buffer_left}$(eval ${__pmy_out_command} | fzf -0 -1)"
+
+        # get result from fzf
+        local fzf_res=$(echo "${__pmy_out_sources}" | fzf -0 -1)
+        LBUFFER="${__pmy_out_buffer_left}${fzf_res}"
         RBUFFER="${__pmy_out_buffer_right}"
     fi
     zle reset-prompt
