ce099c643c29724f6da3b8ab557eca4eb9752636
Hiroki Konishi <hkonishi@yahoo-corp.jp>
2019-06-14 15:25:10 +0900

    Update: implement PMY delimiter by environment variable

--- shell/pmy.zsh
+++ shell/pmy.zsh
@@ -1,7 +1,11 @@
 #!/usr/bin/env zsh
 
+# Export runnable pmy path
 export PATH="${GOPATH}/src/github.com/relastle/pmy:${PATH}"
+
+# Export pmy configuration environment variable
 export PMY_RULE_PATH="${GOPATH}/src/github.com/relastle/pmy/resources/pmy_rules.json"
+export PMY_TAG_DELIMITER="\t"
 
 FF_CMD="fzf -0 -1"
 
@@ -22,12 +26,12 @@ pmy-widget() {
             # get result from fzf
             local fzf_res=$(echo -n "${__pmy_out_sources}" | eval ${FF_CMD})
             # get tag
-            local tag="$(echo -n ${fzf_res} | awk 'BEGIN{ORS = ""}{print $1}' | base64)"
+            local tag="$(echo -n ${fzf_res} | awk -F ${PMY_TAG_DELIMITER} 'BEGIN{ORS = ""}{print $1}' | base64)"
             tag=${tag//\//a_a} # original escape of base64 `/`
             tag=${tag//+/b_b} # original escape of base64 `+`
             tag=${tag//=/c_c} # original escape of base64 `+`
             # get rest statement
-            local fzf_res_rest=$(echo ${fzf_res} | awk '{for(i=2;i<NF;i++){printf("%s%s",$i,OFS=" ")}print $NF}')
+            local fzf_res_rest=$(echo ${fzf_res} | awk -F ${PMY_TAG_DELIMITER} '{for(i=2;i<NF;i++){printf("%s%s",$i,OFS=" ")}print $NF}')
             # get after command
             local after_cmd_variable="__pmy_out_${tag}_after"
             local after_cmd=$(eval echo \$$after_cmd_variable)
