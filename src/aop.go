90bbf026d51382c642bdef935f764efe2415dfe1
Hiroki Konishi <relastle@gmail.com>
2019-10-21 14:32:42 +0900

    Update: mkdir if log directory does not exist

--- src/aop.go
+++ src/aop.go
@@ -3,10 +3,12 @@ package pmy
 import (
 	"log"
 	"os"
+	"path"
 	"time"
 )
 
 func measureElapsedTime(start time.Time, name string) {
+	os.MkdirAll(path.Dir(defaultLogPath), os.ModePerm)
 	f, err := os.OpenFile(os.ExpandEnv(defaultLogPath), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
