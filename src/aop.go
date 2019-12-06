67ba7a3742c28821c52d70942de8026b64db1e69
Hiroki Konishi <hkonishi@yahoo-corp.jp>
2019-12-06 09:54:48 +0900

    Update: remove `taggo` dependencies

--- src/aop.go
+++ src/aop.go
@@ -13,7 +13,7 @@ func MeasureElapsedTime(start time.Time, name string) {
 	logPath := os.ExpandEnv(defaultLogPath)
 	err := os.MkdirAll(path.Dir(logPath), os.ModePerm)
 	if err != nil {
-		panic(err)
+		return
 	}
 	f, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
